d567014aafc3f4c935c21961b0700e5c9501be26
Amit Kumar <amitkcodes@gmail.com>
2025-01-24 23:26:47 +0530

    added 'networks' and 'paths' object in hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,18 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_RPC_KEY}`,
+      accounts: [process.env.METAMASK_PRIVATE_KEY],
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
 };

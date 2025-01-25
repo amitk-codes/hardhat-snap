ab4c8fd7b7f708d003b656c40d46dccee9d38130
Amit Kumar <amitkcodes@gmail.com>
2025-01-25 14:46:05 +0530

    added new task: 'accounts' in hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,18 @@
+const { config: dotEnvConfig } = require("dotenv");
+const { task } = require("hardhat/config");
+dotEnvConfig();
 require("@nomicfoundation/hardhat-toolbox");
+
+task("accounts", "Prints info about the accounts", async (_taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  const providers = hre.ethers.provider;
+
+  for (const account of accounts) {
+    const address = account.address;
+    const balance = await providers.getBalance(address);
+    console.log(`${address} :: ${hre.ethers.formatEther(balance)}`);
+  }
+});
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {

0a4ddf184a335ee3acec2a59ac6503435004b042
Amit Kumar <amitkcodes@gmail.com>
2025-01-25 19:02:48 +0530

    added test case for 'setValue' instruction in the smart contract

--- test/Sample.js
+++ test/Sample.js
@@ -15,4 +15,13 @@ describe("Sample Contract", async () => {
     console.log({ value });
     expect(value).to.equal(1234);
   });
+
+  it("updates the stored number", async () => {
+    const setterTx = await contract.setValue(1111);
+    await setterTx.wait();
+
+    const value = await contract.getValue();
+    console.log({ valueAfterUpdate: value });
+    expect(value).to.equal(1111);
+  });
 });

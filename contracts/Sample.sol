7d6a6b846831788e6aa2c950878f9680f911ee02
Amit Kumar <amitkcodes@gmail.com>
2025-01-25 18:28:58 +0530

    updated the contract with some more functions

--- contracts/Sample.sol
+++ contracts/Sample.sol
@@ -2,10 +2,18 @@
 
 pragma solidity ^0.8.0;
 
-contract Sample{
-  uint storedNum = 12;
+contract Sample {
+    uint storedNum;
 
-  function getStoredNumValue() external view returns(uint){
-    return storedNum;
-  } 
+    constructor(uint _value) {
+        storedNum = _value;
+    }
+
+    function getValue() external view returns (uint) {
+        return storedNum;
+    }
+
+    function setValue(uint _value) external {
+        storedNum = _value;
+    }
 }
